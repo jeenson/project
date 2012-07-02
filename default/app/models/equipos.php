c373c930da317922ba0cee08bb6496e5a1703f0e
JWebSoft <jwebsoft1984@gmail.com>
2012-07-01 21:55:22 -0500

    modulo completo equipos

--- default/app/models/equipos.php
+++ default/app/models/equipos.php
@@ -7,13 +7,28 @@ class Equipos extends ActiveRecord {
         //$this->_belongs_to('paises');
     }
     
-    public function lista_equipos($pagina) {
+    public function listar($pagina , $filtro = NULL) {
+        $condiciones = 'TRUE';
+        if ($filtro) {
+            $condiciones = $this->crear_filtro($filtro);
+        }
         $cols = 'equipos.*, p.nombre as paises';
         $joins = 'INNER JOIN paises as p ON p.id = paises_id ';
-        //$joins .= 'LEFT JOIN menus as m2 ON m2.id = menus.menus_id ';
-        //return $this->paginate("page: $pagina", "columns: $cols", "join: $joins");
-        return $this->paginate("page: $pagina","columns: $cols", "join: $joins");
+        return $this->paginate("page: $pagina","columns: $cols", "join: $joins", "conditions: $condiciones");
     }
+    
+    public function crear_filtro($filtro) {
+        $condiciones = array();
+        if (!empty($filtro['nombre'])) {
+            $condiciones[] = "equipos.nombre LIKE '%{$filtro['nombre']}%'";
+        }
+        if (!empty($filtro['paises_id'])) {
+            $condiciones[] = "equipos.paises_id = '{$filtro['paises_id']}'";
+        }
+        return sizeof($condiciones) ? join(' AND ', $condiciones) : 'TRUE';
+    }
+    
+    
     
 
 }
--- default/app/models/paises.php
+++ default/app/models/paises.php
@@ -1,19 +1,5 @@
 <?php
 
-class Equipos extends ActiveRecord {
-
-    protected function initialize() {
-        //$this->has_many('paises');
-        //$this->_belongs_to('paises');
-    }
-    
-    public function lista_equipos($pagina) {
-        $cols = 'equipos.*, p.nombre as paises';
-        $joins = 'INNER JOIN paises as p ON p.id = paises_id ';
-        //$joins .= 'LEFT JOIN menus as m2 ON m2.id = menus.menus_id ';
-        //return $this->paginate("page: $pagina", "columns: $cols", "join: $joins");
-        return $this->paginate("page: $pagina","columns: $cols", "join: $joins");
-    }
-    
+class Paises extends ActiveRecord {
 
 }
